c14afbf47d57f075aa2b296ec958e1d9ecbc89e9
Ryan Lo <ryan.lo888@gmail.com>
2022-08-02 17:06:59 -0400

    Added more sections

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Contact from '@/components/Contact';
 import Head from 'next/head'
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
+import Education from '@/components/Education';
+import Experience from '@/components/Experience';
 import Stack from '@/components/Stack';
 import { meta } from '@/data/config';
 
